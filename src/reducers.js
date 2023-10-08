d21bfaeebdc12b7c3902b0c4f7ce2bb18e1dd82f
Tejas Dupare <tejasdupare786@.gmail.com>
2023-10-08 14:05:03 +0530

    fuel wallet

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,31 @@
 import { v4 as uuidv4 } from "uuid";
 
-const reducer = (state, action) => {};
+
+const reducer = (state , action) => {
+  switch (action.type){
+    case 'add':
+        const newUser = {
+            id: uuidv4(),
+            name: action.name,
+            wallet: state.balance +10,
+        };
+        return {
+            ...state,
+            users: [...state.users, newUser],
+            balance: state.balance + 10,
+        };
+        case 'fuel':
+            if(state.balance >= 50){
+                return {
+                    ...state,
+                    balance: state.balance - 50,
+                };
+            }else{
+                return state;
+            }
+            default:
+                return state;
+  }
+};
 
 export default reducer;
